d6766ca587334e3083996a85cf3176715af21e22
Christian Schell <mail@chrisschell.de>
2013-03-31 19:53:37 +0200

    adds smtp_production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Potter::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.default_url_options = { :host => 'potter.sophisticates.de' }
+  config.action_mailer.delivery_method = :smtp
 end
---
+++ config/initializers/smtp_production.sample.rb
@@ -0,0 +1,11 @@
+# if Rails.env == "production"
+#   ActionMailer::Base.smtp_settings = {
+#     :user_name => "username",
+#     :password => "password",
+#     :domain => "domain.com",
+#     :address => "smtp.domain.net",
+#     :port => 25,
+#     :authentication => :plain,
+#     :enable_starttls_auto => true
+#   }
+# end
