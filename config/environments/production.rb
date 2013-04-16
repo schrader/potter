7fd807ad843e3fd694f953c6efe1a4ec050e000e
Christian Schell <mail@chrisschell.de>
2013-04-16 21:47:04 +0200

    updates production config to also precompile main.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Potter::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( search.js main.css.scss)
+  config.assets.precompile += %w(main.css)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
