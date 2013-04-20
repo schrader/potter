387c3aebd9b4efaf71e2e12d9f5312800cada404
Christian Schell <mail@chrisschell.de>
2013-04-20 12:59:00 +0200

    change pot mailer's 'from'

--- app/mailers/pot_mailer.rb
+++ app/mailers/pot_mailer.rb
@@ -1,5 +1,5 @@
 class PotMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "hedwig@sophisticates.de"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
