782ede92a42e22a1ed4d24a2f52e89b45e0a1e0b
Christian Schell <mail@chrisschell.de>
2013-04-20 12:04:18 +0200

    nicer 'new pot' link

--- app/helpers/pots_helper.rb
+++ app/helpers/pots_helper.rb
@@ -1,2 +1,6 @@
 module PotsHelper
+  def random_illustration
+    illustrations = %w(bag.png book.png calendar.png  clipboard.png colors.png compass.png gift.png infinity.png  mail.png map.png paper.png retina.png share.png time.png)
+    raw image_tag("/illustrations/#{illustrations.sample}", class: "illustrations")
+  end
 end
