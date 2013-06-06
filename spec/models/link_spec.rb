abed3fd9eaddbbe979f96354b8082bdf66b4a4a1
Christian Schell <mail@chrisschell.de>
2013-06-06 16:55:13 -0400

    adds spec for Link.markoff

--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Link do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".markoff" do
+    it "sets sent_at to current time for the collection of links" do
+      Timecop.freeze
+      Link.should_receive(:update_all).with(sent_at: Time.zone.now)
+      Link.markoff
+    end
+  end
 end
