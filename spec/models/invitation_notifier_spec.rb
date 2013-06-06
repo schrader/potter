d367310ebf125292aaf5eef4525d2cbed22d7c7d
Christian Schell <mail@chrisschell.de>
2013-06-06 17:04:05 -0400

    adds specs to invitations notifier

--- spec/models/invitation_notifier_spec.rb
+++ spec/models/invitation_notifier_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe InvitationNotifier do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#notify" do
+    it "sends an email" do
+      UserMailer.should_receive(:invite).with(instance_of(Invitation)).and_return(OpenStruct.new(deliver: true))
+      
+      InvitationNotifier.new(Invitation.new).notify
+    end
+  end
 end
