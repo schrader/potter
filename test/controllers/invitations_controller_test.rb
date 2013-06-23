0f343a1a7e27d55663a7eac671b5defae88f21b4
Christian Schell <mail@chrisschell.de>
2013-06-23 11:48:51 +0200

    ports InvitationsController specs to unit tests

---
+++ test/controllers/invitations_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class InvitationsControllerTest < ActionController::TestCase
+  def setup
+    sign_in create(:user)
+  end
+  
+  test "POST #create invites user and redirects to invitation's pot" do
+    Pot.stubs(:find).returns(build_stubbed(:pot, id: 42))
+    InvitationNotifier.any_instance.expects(:notify).once
+
+    post :create, {invitation: {email: "mail@example.com"}, pot_id: 42}
+    
+    assert_redirected_to pot_path(42)
+  end
+
+  test "GET #accept accepts invitation and redirects to invitation's pot" do
+    @invitations_pot = build_stubbed(:pot, id: 42)
+    Invitation.stubs(:find_by_token).returns(build_stubbed(:invitation, pot: @invitations_pot))
+    Invitation.any_instance.expects(:accept!).once.returns(true)
+
+    get :accept, {pot_id: @invitations_pot.id}
+
+    assert_redirected_to pot_path(@invitations_pot)
+  end
+end
