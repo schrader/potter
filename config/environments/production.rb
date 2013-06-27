df27aaeb8f8ef22f00ee235ba963bf786e3ed4b4
Christian Schell <mail@chrisschell.de>
2013-06-27 16:38:43 +0200

    compile assets while deploying

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Potter::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w(main.css *.png *.jpg *.jpeg *.gif)
+  config.assets.precompile += %w(main.css *.png *.jpg *.jpeg *.gif *.woff *.ttf *.svg)
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
