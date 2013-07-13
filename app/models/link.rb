67ea73db8dd46ada07fa585480b5dbd97fd45e80
Christian Schell <mail@chrisschell.de>
2013-07-13 10:27:17 +0200

    new scope for Link to catch one week's links ugly scope, I hope we can find more elegant version

--- app/models/link.rb
+++ app/models/link.rb
@@ -8,6 +8,7 @@ class Link < ActiveRecord::Base
   
   scope :from_user, lambda {|user| where("user_id = ?", user.id)}
   scope :from_others_than, lambda {|user| where("user_id != ?", user.id)}
+  scope :sent_directly_after, -> time { where(sent_at: where("sent_at >= ?", time.to_datetime).order("sent_at ASC").limit(1).pluck(:sent_at))}
 
   validates :user, presence: true
   validates :pot, presence: true
