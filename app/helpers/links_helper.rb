5d2fa7df08bf8a974257acb2c838219b3f5fe9b6
Christian Schell <mail@chrisschell.de>
2013-07-14 17:25:20 +0200

    new Archive pagination

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,21 @@
 module LinksHelper
+  def archive_weeks
+    Link.where(pot_id: current_pot).uniq(:sent_at).order(sent_at: :desc).pluck(:sent_at).compact.map{|date| LinksController::CalendarWeek.new(date)}
+  end
+
+  def archive_weeks_grouped_by_years_and_months
+    years = archive_weeks_grouped_by_years
+    archive_weeks_grouped_by_years.each do |year, weeks|
+      years[year] = weeks.group_by do |week|
+        week.strftime("%B")
+      end
+    end
+    years
+  end
+
+  def archive_weeks_grouped_by_years
+    archive_weeks.group_by do |week|
+      week.strftime("%Y")
+    end
+  end
 end
