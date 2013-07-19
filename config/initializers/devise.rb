39194c1cdc3a16d01b6b68905c44e207191ec164
Christian Schell <mail@chrisschell.de>
2013-07-19 13:23:42 +0200

    updates devise config to remember passwords for 2 months and use hello@potterapp.de

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "hello@potter.sophisticates.de"
+  config.mailer_sender = "hello@potterapp.de"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
@@ -111,7 +111,7 @@ Devise.setup do |config|
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
-  # config.remember_for = 2.weeks
+  config.remember_for = 2.months
 
   # If true, extends the user's remember period when remembered via cookie.
   # config.extend_remember_period = false
