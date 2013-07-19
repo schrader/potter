ebeb18469a238dfb83d2d8bb5f5bd9232f20efdb
Christian Schell <mail@chrisschell.de>
2013-07-19 12:52:10 +0200

    updates unicorn config to listen for new domain potterapp.de

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,9 +1,9 @@
-working_directory "/home/deploy/ruby_on_rails/potter.sophisticates.de/current/"
-pid "/home/deploy/ruby_on_rails/potter.sophisticates.de/current/tmp/pids/unicorn.pid"
-stderr_path "/home/deploy/ruby_on_rails/potter.sophisticates.de/shared/log/unicorn.log"
-stdout_path "/home/deploy/ruby_on_rails/potter.sophisticates.de/shared/log/unicorn.log"
+working_directory "/home/deploy/ruby_on_rails/potterapp.de/current/"
+pid "/home/deploy/ruby_on_rails/potterapp.de/current/tmp/pids/unicorn.pid"
+stderr_path "/home/deploy/ruby_on_rails/potterapp.de/shared/log/unicorn.log"
+stdout_path "/home/deploy/ruby_on_rails/potterapp.de/shared/log/unicorn.log"
 
-listen "/tmp/unicorn.potter.sophisticates.de.sock"
+listen "/tmp/unicorn.potterapp.de.sock"
 worker_processes 2
 timeout 30
 
